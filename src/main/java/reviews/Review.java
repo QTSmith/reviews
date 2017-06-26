7606e3ed23858f94c40fecb29c071eb198cca09c
Quiana <qtsmith.thedev@gmail.comgit config --global user.email you@example.comgit config --global user.email qtsmith.thedev@gmail.com>
2017-06-26 08:25:06 -0400

    folder issues

--- src/main/java/reviews/Review.java
+++ src/main/java/reviews/Review.java
@@ -1,8 +1,8 @@
 package reviews;
 
 public class Review {
-	
-	//Instance variables
+
+	// Instance variables
 	private long id;
 	private String breed;
 	private String imageUrl;
@@ -10,40 +10,47 @@ public class Review {
 	private String opinion;
 	private String funfacts;
 	private String tag;
-	
-	//Constructor
-	public Review(long id, String breed, String imageUrl, String cuteness, String opinion, String funfacts, String tag){
-	 this.id = id;
-	 this.breed = breed;
-	 this.imageUrl = imageUrl;
-	 this.cuteness = cuteness;
-	 this.opinion = opinion;
-	 this.funfacts = funfacts;
-	 this.tag = tag;
-	 
+
+	// Constructor
+	public Review(long id, String breed, String imageUrl, String cuteness, String opinion, String funfacts,
+			String tag) {
+		this.id = id;
+		this.breed = breed;
+		this.imageUrl = imageUrl;
+		this.cuteness = cuteness;
+		this.opinion = opinion;
+		this.funfacts = funfacts;
+		this.tag = tag;
+
 	}
-	//Getters
+
+	// Getters
 	public long getId() {
 		return id;
 	}
+
 	public String getTitle() {
 		return breed;
 	}
+
 	public String getImageUrl() {
 		return imageUrl;
 	}
+
 	public String getCuteness() {
 		return cuteness;
 	}
+
 	public String getOpinion() {
 		return opinion;
 	}
-		public String getFunFacts() {
+
+	public String getFunFacts() {
 		return funfacts;
 	}
+
 	public String getTag() {
 		return tag;
 	}
-	
 
 }
